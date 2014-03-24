a09ddc2655baa9eb0d7bd1d7b43bd3f78aa422ff
NeophytePHP <j.w.h.m.1997@hotmail.co.uk>
2014-03-24 18:57:38 +0000

    Updated

--- exec/exec.php
+++ exec/exec.php
@@ -1,6 +1,17 @@
 <?php  
 	require("exec_cmd_config.php");
 	error_reporting(0);
+	$allowed_groups = array((array_values($setup[4])));
+		$can_see = FALSE;
+		foreach ($allowed_groups as $allowed)
+			if (in_array($allowed, $user_info['groups']))
+			{
+				$can_see = TRUE;
+				break;
+			}
+			
+		if ($can_see)
+		{
 		if(isset($_POST['command'])){
 			if(!empty($_POST['command'])){
 				if(isset($_POST['quickCmd'])){
@@ -17,5 +28,11 @@
 				$r->rconCommand($execute);
 				echo 'Executed: "' . $execute . '" successfully...';
 			}
+		}
+		}
+		else
+		{
+			echo $setup[5];
+			redirectexit($setup[6]);
 		}
 ?>
